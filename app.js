d60896dc6b92f4949a942f80ecd1252d7caa2dfe
Sameer <sameer@Sameers-MacBook-Pro.local>
2021-06-14 13:29:37 +0530

    pokeapi complete

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const container = document.querySelector('#container')
 const baseurl ='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
 for (let i = 1; i < 10; i++) {
+    const pokemon =document.createElement('div')
+    const label =document.createElement('span')
+    pokemon.classList.add('pokemon')
+    label.innerText=`#${i}`
     const newIMG = document.createElement('img')
     newIMG.src = `${baseurl}${i}.png`
-    container.appendChild(newIMG)
-
+    pokemon.appendChild(newIMG)
+    pokemon.appendChild(label)
+    container.appendChild(pokemon)
 
 }
